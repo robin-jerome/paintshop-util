7d550699638912b22d4e56bda6e6504a11b8618f
Robin Jerome <robin.jerome@outlook.com>
2016-04-25 22:05:18 +0300

    Add more test cases to Catalog Builder

--- src/test/java/com/paintshop/CatalogBuilderTest.java
+++ src/test/java/com/paintshop/CatalogBuilderTest.java
@@ -1,23 +1,36 @@
 package com.paintshop;
 
 import com.google.common.collect.ImmutableList;
-import com.paintshop.model.ColorFinish;
-import com.paintshop.model.Customer;
-import com.paintshop.model.CustomerWish;
-import com.paintshop.model.Product;
+import com.paintshop.file.InputReader;
+import com.paintshop.model.*;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
 public class CatalogBuilderTest {
-
     private static final CatalogBuilder BUILDER = new CatalogBuilder();
+    private static final String TEST_RESOURCES_FILE_LOC = "src" + File.separator + "test" + File.separator + "resources" + File.separator;
 
     @Test
-    public void testBuildCatalog() throws Exception {
+    public void testBuildCatalogForValidSolution() throws Exception {
+        TestCase tc = new InputReader(TEST_RESOURCES_FILE_LOC + "input_with_soln.txt").getTestCases().get(0);
+        Optional<List<Product>> catalogOpt = BUILDER.buildCatalog(tc);
+        assertTrue("There should be a valid product catalog", catalogOpt.isPresent());
+        String catalogRepresentation = catalogOpt.get().stream()
+                .map(p -> "" + p.getColorFinish().getCode()).collect(Collectors.joining(" "));
+        assertEquals("Catalog representation is incorrect", "1 0 0 0 0", catalogRepresentation);
+    }
 
+    @Test
+    public void testBuildCatalogForNonExistingSolution() throws Exception {
+        TestCase tc = new InputReader(TEST_RESOURCES_FILE_LOC + "input_with_no_soln.txt").getTestCases().get(0);
+        Optional<List<Product>> catalogOpt = BUILDER.buildCatalog(tc);
+        assertFalse("There should be no product catalog", catalogOpt.isPresent());
     }
 
     @Test
